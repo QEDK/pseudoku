c2e9f85b3b2ad67766b820fd311dd0eeecfd6730
qedk <1994constant@gmail.com>
2025-08-26 17:46:17 +0400

    feat: add terser and lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,39 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import { cloudflare } from "@cloudflare/vite-plugin";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
-  root: '.',
+  root: ".",
   build: {
-    target: 'esnext',
-    outDir: 'dist',
+    target: "esnext",
+    outDir: "dist",
+    chunkSizeWarningLimit: 4000,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
+        main: resolve(__dirname, "index.html"),
       },
       output: {
         manualChunks: {
-          'noir': ['@noir-lang/noir_js'],
-          'bb': ['@aztec/bb.js'],
-          'noirc_abi': ['@noir-lang/noirc_abi'],
-          'acvm_js': ['@noir-lang/acvm_js'],
-        }
-      }
-    }
+          noir: ["@noir-lang/noir_js"],
+          bb: ["@aztec/bb.js"],
+          noirc_abi: ["@noir-lang/noirc_abi"],
+          acvm_js: ["@noir-lang/acvm_js"],
+        },
+      },
+    },
+    minify: "terser",
   },
   optimizeDeps: {
     esbuildOptions: {
-      target: "esnext"
+      target: "esnext",
     },
-    exclude: ['@noir-lang/noirc_abi', '@noir-lang/acvm_js']
+    exclude: ["@noir-lang/noirc_abi", "@noir-lang/acvm_js"],
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src')
-    }
+      "@": resolve(__dirname, "./src"),
+    },
   },
   plugins: [
     nodePolyfills({
@@ -44,8 +46,8 @@ export default defineConfig({
   server: {
     port: 5173,
     headers: {
-      'Cross-Origin-Embedder-Policy': 'require-corp',
-      'Cross-Origin-Opener-Policy': 'same-origin'
-    }
-  }
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
+    },
+  },
 });
